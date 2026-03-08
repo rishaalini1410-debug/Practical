33122d8dfc32144463f05e692282d430d96ffb02
rishaalini1410-debug <rishaalini1410@gmail.com>
2026-03-08 22:38:16 +0800

    Add files via upload

--- Q1-PRAC5.py
+++ Q1-PRAC5.py
@@ -3,9 +3,11 @@ import streamlit as st
 st.title("Taylor's Student")
 
 st.header("About Me")
+st.image("goofy cat.jpg", caption="believe me. cats deserve education too. Me able to read.")
 
 st.text("I am a biotechnology student.")
-st.text("I love food")
+st.text("I love food.")
 st.text("I don't have a favourite colour but I think pink or blue is not bad.")
 
 st.success("Welcome to my first Streamlit app!")
+
